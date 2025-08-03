025b7a7bb54cb0da297b9ed533ebb445be185b18
gastonfalena <gastonfalena@gmail.com>
2025-08-02 23:53:32 -0300

    add: function initGame

--- javacript/game.js
+++ javacript/game.js
@@ -11,3 +11,31 @@ var timerInterval;
 var seconds = 0;
 var playerName = "";
 var difficulty = "easy";
+
+function initGame() {
+  board = [];
+  flaggedCount = 0;
+  revealedCount = 0;
+  gameOver = false;
+  firstClick = true;
+  clearInterval(timerInterval);
+  seconds = 0;
+
+  boardSize = difficultySettings[difficulty].size;
+  mineCount = difficultySettings[difficulty].mines;
+
+  for (var i = 0; i < boardSize; i++) {
+    board[i] = [];
+    for (var j = 0; j < boardSize; j++) {
+      board[i][j] = {
+        isMine: false,
+        isRevealed: false,
+        isFlagged: false,
+        value: 0,
+      };
+    }
+  }
+
+  updateMinesCounter();
+  updateTimer();
+}
